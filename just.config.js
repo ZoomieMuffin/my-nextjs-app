e1e6e8ed8f23c5fe028499ec03f4094081cc71b9
Mochifuwa <mochifuwa@Mochifuwas-Mac-mini.local>
2025-04-09 13:04:55 +0900

    add .babelrc

--- just.config.js
+++ just.config.js
@@ -1,4 +1,12 @@
+// jest.config.js
 module.exports = {
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+    transform: {
+      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+    },
+    moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+    moduleNameMapper: {
+      "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    }
   };
