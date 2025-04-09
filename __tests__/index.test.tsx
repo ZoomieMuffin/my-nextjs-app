9da06d9afb3e6e964645539bc831e89353b989b2
Mochifuwa <mochifuwa@Mochifuwas-Mac-mini.local>
2025-04-09 13:00:38 +0900

    Add GitHub Actions CI

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,11 @@
+// __tests__/index.test.tsx（または .js）
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+    const heading = screen.getByRole("heading", { name: /welcome/i });
+    expect(heading).toBeInTheDocument();
+  });
+});
